092e2a2101f3bba43f8b79b70b58a04e1c59f959
Mohd Saayem <121473537+Saayem123@users.noreply.github.com>
2025-01-26 13:31:56 +0530

    deploy docker image

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,16 +1,3 @@
-terraform {
-    required_providers {
-        aws = {
-            source = "hashicorp/aws"
-            version = "~> 5.0"
-        }
-    }
-
-    backend "s3" {
-        bucket = "task-manager-terraform-state"
-        key = "aws/ec2-deploy/terraform.tfstate"
-    }
-}
 
 provider "aws" {
     region = var.region
